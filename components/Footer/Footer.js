6d681c8367e393917f1d625b828418e0591f5700
Jan Raadts <janraadts@outlook.de>
2022-12-02 20:42:16 +0000

    styled footer and logo

---
+++ components/Footer/Footer.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import Image from "next/image";
+import add from "/Users/janraadts/Development/neuefische/spritty/components/Footer/fluent_save-copy-24-regular.png";
+import bib from "/Users/janraadts/Development/neuefische/spritty/components/Footer/bib.png";
+
+export default function Footer() {
+  return (
+    <StyledFooter>
+      <Image src={add} alt="add_btn" width={50} />
+      <Image src={bib} alt="bib_btn" width={40} />
+    </StyledFooter>
+  );
+}
+
+const StyledFooter = styled.section`
+  position: fixed;
+  bottom: 0;
+  height: 80px;
+  background-color: #11223e;
+  width: 100%;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 70px;
+  padding-right: 70px;
+`;
